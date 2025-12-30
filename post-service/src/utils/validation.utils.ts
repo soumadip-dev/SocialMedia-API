8ec0f9f1f212f229d1af59406516a9090f424c63
soumadip-dev <soumadipmajila@gmail.com>
2025-12-30 22:59:53 +0530

    feat: make mediaIds optional and validate as string array

--- post-service/src/utils/validation.utils.ts
+++ post-service/src/utils/validation.utils.ts
@@ -2,11 +2,13 @@ import Joi, { ValidationResult } from 'joi';
 
 interface PostDTO {
   content: string;
+  mediaIds?: string[];
 }
 
 const validatePost = (data: PostDTO): ValidationResult => {
   const schema = Joi.object<PostDTO>({
     content: Joi.string().min(3).max(5000).required(),
+    mediaIds: Joi.array().items(Joi.string()).optional(),
   });
 
   return schema.validate(data);
