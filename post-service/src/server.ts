cd2949aa11419edafb35ee83b9ce848363738f65
soumadip-dev <soumadipmajila@gmail.com>
2025-12-31 19:32:46 +0530

    refactor: handle startup errors and connect to RabbitMQ before server start

--- post-service/src/server.ts
+++ post-service/src/server.ts
@@ -2,33 +2,44 @@ import app from './app.js';
 import { connectDB } from './config/db.config.js';
 import { ENV } from './config/env.config.js';
 import logger from './utils/logger.utils.js';
+import { connectToRabbitMQ } from './utils/rabbitmq.utils';
 
 const PORT = ENV.PORT;
 
 // Handle unhandled promise rejections
 process.on('unhandledRejection', (reason, promise) => {
-  logger.error(`Unhandled Rejection ❌ at ${promise}, Reason: ${reason} `);
+  logger.error(`Unhandled Rejection ❌ at ${promise}, Reason: ${reason}`);
+  process.exit(1);
 });
 
 const startServer = async () => {
-  connectDB();
+  try {
+    // Connect to DB
+    await connectDB();
 
-  // Start the server
-  const server = app.listen(PORT, () => {
-    logger.info(`Post service running on http://localhost:${PORT} 🌐`);
-  });
+    // Connect to RabbitMQ
+    await connectToRabbitMQ();
 
-  // Handle server errors
-  server.on('error', err => {
-    if ('code' in err && err.code === 'EADDRINUSE') {
-      logger.error(
-        `Port ${PORT} is already in use. Please stop the running process or use a different port. ⚠️`
-      );
-    } else {
-      logger.error('Failed to start server ❌', { error: err });
-    }
+    // Start the server
+    const server = app.listen(PORT, () => {
+      logger.info(`Post service running on http://localhost:${PORT} 🌐`);
+    });
+
+    // Handle server errors
+    server.on('error', err => {
+      if ('code' in err && err.code === 'EADDRINUSE') {
+        logger.error(
+          `Port ${PORT} is already in use. Please stop the running process or use a different port. ⚠️`
+        );
+      } else {
+        logger.error('Failed to start server ❌', { error: err });
+      }
+      process.exit(1);
+    });
+  } catch (error) {
+    logger.error('Application startup failed', { error });
     process.exit(1);
-  });
+  }
 };
 
 startServer();
